7f133f22e917fa61a6af8f14b7991abefb761193
Yee Ching Hui <eugenehui93@Yees-MBP.lan>
2023-03-11 11:41:22 -0800

    add images

---
+++ src/components/EmptySlot.jsx
@@ -0,0 +1,24 @@
+
+import { useEffect } from "react"
+import MoleHill from '../molehill.png'
+
+const EmptySlot = (props) => {
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 7000)
+        let timer = setTimeout(() => {
+            props.toggle(true)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div>
+            <img
+                style={{ 'width': '30vw' }}
+                src={MoleHill}
+                alt={[]} />
+        </div>
+    )
+}
+
+export default EmptySlot
